74c76057ae46d8535875ad0c70b49369f605f058
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-09-17 23:45:36 +0800

    first update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      React homework template tadonga kat kay mahal ang tuition
     </div>
   );
 };
