2c87ebc93df6881e008eb28147d74332d919117c
katrinajy7 <145580105+katrinajy7@users.noreply.github.com>
2024-10-21 13:38:26 +0800

    updatedapi

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.goit.global';
+axios.defaults.baseURL = 'https://nodejs-homework-template-2.onrender.com';
 
 const setAuthHeader = token => {
     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
